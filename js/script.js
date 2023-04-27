e3fc3a4b55361567e622c47f260441e784dd5055
Taibi Mohamed Kamel Eddine <tmkev1.0@gmail.com>
2023-04-27 21:16:20 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      centeredSlides: false,
+      slidesPerGroupSkip: 0,
+      grabCursor: true,
+      loop: false,
+      keyboard: {
+        enabled: true,
+      },
+      breakpoints: {
+        769: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+        },
+      },
+      // scrollbar: {
+      //   el: ".swiper-scrollbar",
+      // },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
